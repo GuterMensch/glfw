6a7e0b7e334208501de1b799c65cc5d234022812
Thomas Conraths <thomas.conraths@rwth-aachen.de>
2019-05-08 19:32:10 +0200

    Cleaned premake5.lua file

--- premake5.lua
+++ premake5.lua
@@ -3,6 +3,7 @@
 project "GLFW"
 	kind "StaticLib" -- static library
 	language "C"
+	staticruntime "on"
 	
 	targetdir ("../../../bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("../../../bin-int/" .. outputdir .. "/%{prj.name}")
@@ -22,7 +23,6 @@ project "GLFW"
 	
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
 		
 		files
 		{
@@ -43,5 +43,10 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 		
-	filter {"system:windows", "configurations:Release"}
-		buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+		
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
